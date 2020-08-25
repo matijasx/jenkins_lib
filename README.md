653ba476519f87f6e9daac2bb731d33a707e0c8a
Your Name <matijassx@gmail.com>
2020-08-25 11:55:49 +0300

    Fixed folder structure

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains shared libraries used to construct pipelines.
--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,3 +1,7 @@
-def call(String name = 'Default Application name'){
-	echo "Hello ${name}."
+def call(String buildUsing = 'docker', String image = ''){
+	if (buildUsing == 'docker' {
+		sh 'Building using ${buildUsing} and image ${image}'
+	} else {
+		sh 'echo  "buildApp method missing either buildUsing or image arguments"'
+	}
 }
