f82895ec445e225de0a14f24701033d5b0fa1e6f
Your Name <matijassx@gmail.com>
2020-08-25 18:32:48 +0300

    Added more docker commands

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -5,6 +5,8 @@ def call(String buildUsing = 'docker', image = ''){
 	if (buildUsing == 'docker' && image) {
 		echo "Building using ${buildUsing} and image ${image}"
 		sh 'docker build . -t apache_app'
+		sh 'docker images'
+		sh 'docker ps'
 	} else {
 		error "buildApp method missing one or more arguments"
 	}
