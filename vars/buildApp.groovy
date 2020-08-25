f92655d1746c362b34d2c3d4aac246e0f87822c1
Your Name <matijassx@gmail.com>
2020-08-25 18:25:15 +0300

    Added Dockerfile to build app

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -4,6 +4,7 @@ def call(String buildUsing = 'docker', image = ''){
 	echo "Building using ${buildUsing} and image ${image}"
 	if (buildUsing == 'docker' && image) {
 		echo "Building using ${buildUsing} and image ${image}"
+		sh 'docker build . -t '
 	} else {
 		error "buildApp method missing one or more arguments"
 	}
