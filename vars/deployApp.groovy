b3fea8c0da6071f49f4d6575dce9907616a23ea3
Your Name <matijassx@gmail.com>
2020-08-25 19:23:26 +0300

    Added parameters to deploy stage

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String serviceName = '', externalPort = '80',  internalPort = '80', image = ''){
-	if (serviceName && image){
+def call(String provisioner = 'docker' serviceName = '', externalPort = '80',  internalPort = '80', image = ''){
+	echo "Deploying"
+	if (provisioner == 'docker' && serviceName && image){
 
 		sh "docker run -d --name ${serviceName} --port ${port} ${image}"
 	}
