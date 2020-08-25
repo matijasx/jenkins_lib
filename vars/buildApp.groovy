1b54993ddaa68103bffb11a7bc9f6084c7fde365
Your Name <matijassx@gmail.com>
2020-08-25 19:35:07 +0300

    Added builtImageName parameter

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String buildUsing = 'docker', image = ''){
+def call(String buildUsing = 'docker', image = '', builtImageName = ''){
 	echo "Building using ${buildUsing} and image ${image}"
-	if (buildUsing == 'docker' && image) {
+	if (buildUsing == 'docker' && image && builtImageName) {
 		echo "Building using ${buildUsing} and image ${image}"
-		sh 'docker build . -t apache_app'
+		sh 'docker build . -t ${builtImageName}'
 		sh 'docker images'
 		sh 'docker ps'
 	} else {
