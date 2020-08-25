512b47aed8d42d6fd7eb017615a04baf6ff65a49
Your Name <matijassx@gmail.com>
2020-08-25 12:02:13 +0300

    Extending build library

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,10 @@
+#!/usr/bin/env groovy
+
 def call(String buildUsing = 'docker', String image = ''){
-	if (buildUsing == 'docker' {
+	echo 'Building using ${buildUsing} and image ${image}'
+	/*if (buildUsing == 'docker' {
 		sh 'Building using ${buildUsing} and image ${image}'
 	} else {
 		sh 'echo  "buildApp method missing either buildUsing or image arguments"'
-	}
+	}*/
 }
