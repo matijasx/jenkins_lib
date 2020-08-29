4a405e16e738f5a2625219a88c0eabfbc79fcb97
Your Name <matijassx@gmail.com>
2020-08-29 15:30:12 +0300

    Added else statement

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,8 +3,9 @@
 def call(String provisioner = 'docker', serviceName = '', externalPort = '80',  internalPort = '80', image = ''){
 	echo "Deploying"
 	if (provisioner == 'docker' && serviceName && image){
-
 		sh "docker run -d --name ${serviceName} -p ${externalPort}:${internalPort} ${image}"
 	}
-	
+	else {
+		error "buildApp method missing one or more arguments"
+	}
 }
